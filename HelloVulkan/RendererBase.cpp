1cdf8a692100ca8bae1447ae96e41c5959b21734
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-15 19:41:33 -0500

    ca create pipeline

--- HelloVulkan/RendererBase.cpp
+++ HelloVulkan/RendererBase.cpp
@@ -315,6 +315,8 @@ bool RendererBase::CreateGraphicsPipeline(
 	//};
 	VkPipelineVertexInputStateCreateInfo vertexInputInfo;
 	vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+	vertexInputInfo.pNext = nullptr;
+	vertexInputInfo.flags = 0;
 	vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
 	vertexInputInfo.vertexBindingDescriptionCount = static_cast<uint32_t>(bindingDescriptions.size());
 	vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
