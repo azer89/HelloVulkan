d5946001484a1282c71b54c9ef359f9b4f39a502
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-20 08:06:06 -0500

    init

---
+++ HelloVulkan/RendererEquirect2Cubemap.cpp
@@ -0,0 +1,11 @@
+#include "RendererEquirect2Cubemap.h"
+
+RendererEquirect2Cubemap::RendererEquirect2Cubemap(VulkanDevice& vkDev)
+{
+
+}
+
+RendererEquirect2Cubemap::~RendererEquirect2Cubemap()
+{
+
+}
---
+++ HelloVulkan/RendererEquirect2Cubemap.h
@@ -0,0 +1,19 @@
+#ifndef RENDERER_EQUIRECT_2_CUBEMAP
+#define RENDERER_EQUIRECT_2_CUBEMAP
+
+#include "RendererBase.h"
+#include "VulkanTexture.h"
+
+#include "glm/glm.hpp"
+
+class RendererEquirect2Cubemap : public RendererBase
+{
+public:
+	RendererEquirect2Cubemap(VulkanDevice& vkDev);
+	~RendererEquirect2Cubemap();
+
+private:
+	std::vector<VkDescriptorSet> descriptorSets_;
+};
+
+#endif
