f4cf34dd7a7bf5e111ddae110c9442572fa22be5
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-21 14:54:22 -0500

    wip

--- HelloVulkan/RendererIrradiance.h
+++ HelloVulkan/RendererIrradiance.h
@@ -1,9 +1,20 @@
 #ifndef RENDERER_IRRADIANCE
 #define RENDERER_IRRADIANCE
 
-class RendererIrradiance
-{
+#include "RendererBase.h"
+#include "VulkanTexture.h"
 
+class RendererIrradiance final : public RendererBase
+{
+	RendererIrradiance(VulkanDevice& vkDev);
+	~RendererIrradiance();
+
+	void OfflineRender(VulkanDevice& vkDev, VulkanTexture* cubemapTexture, VulkanTexture* irradianceTexture);
+	virtual void FillCommandBuffer(VkCommandBuffer commandBuffer, size_t currentImage) override;
+
+private:
+	VkDescriptorSet descriptorSet_;
+	VkFramebuffer frameBuffer_;
 };
 
 #endif
