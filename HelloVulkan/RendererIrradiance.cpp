57d56c5847f49f9d82ab771a0c72046490ae7983
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-21 15:40:54 -0500

    created image views

--- HelloVulkan/RendererIrradiance.cpp
+++ HelloVulkan/RendererIrradiance.cpp
@@ -1,5 +1,11 @@
 #include "RendererIrradiance.h"
 
+// Irradiance map has only one mipmap level
+const uint32_t mipmapCount = 1u;
+const uint32_t cubemapSideLength = 1024;
+const VkFormat cubeMapFormat = VK_FORMAT_R32G32B32A32_SFLOAT;
+const uint32_t layerCount = 6;
+
 RendererIrradiance::RendererIrradiance(
 	VulkanDevice& vkDev) :
 	RendererBase(vkDev, nullptr)
@@ -13,4 +19,27 @@ RendererIrradiance::~RendererIrradiance()
 
 void RendererIrradiance::FillCommandBuffer(VkCommandBuffer commandBuffer, size_t currentImage)
 {
+}
+
+void RendererIrradiance::InitializeIrradianceTexture(VulkanDevice& vkDev, VulkanTexture* irradianceTexture)
+{
+	irradianceTexture->image_.CreateImage(
+		vkDev.GetDevice(),
+		vkDev.GetPhysicalDevice(),
+		cubemapSideLength,
+		cubemapSideLength,
+		mipmapCount,
+		layerCount,
+		cubeMapFormat,
+		VK_IMAGE_TILING_OPTIMAL,
+		VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+		VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT
+	);
+}
+
+void RendererIrradiance::OfflineRender(VulkanDevice& vkDev, VulkanTexture* cubemapTexture, VulkanTexture* irradianceTexture)
+{
+	InitializeIrradianceTexture(vkDev, irradianceTexture);
+
 }
