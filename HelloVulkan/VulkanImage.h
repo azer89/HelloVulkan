a2913e5158a249f740e913c85914f937cc1bb37f
Reza Saputra <radhitya@uwaterloo.ca>
2023-12-22 11:27:02 -0500

    wip

--- HelloVulkan/VulkanImage.h
+++ HelloVulkan/VulkanImage.h
@@ -20,6 +20,8 @@ public:
 	uint32_t layerCount_;
 	VkFormat imageFormat_;
 
+public:
+
 	VulkanImage() :
 		image_(nullptr),
 		imageMemory_(nullptr),
@@ -32,7 +34,6 @@ public:
 	{
 	}
 
-public:
 	bool CreateImage(
 		VkDevice device,
 		VkPhysicalDevice physicalDevice,
@@ -90,6 +91,94 @@ public:
 		VkImageLayout newLayout,
 		uint32_t layerCount = 1,
 		uint32_t mipLevels = 1);
+
+	void GenerateMipmap(
+		VulkanDevice& vkDev,
+		uint32_t maxMipLevels,
+		uint32_t width,
+		uint32_t height,
+		VkImageLayout currentImageLayout
+	)
+	{
+		VkCommandBuffer commandBuffer = vkDev.BeginSingleTimeCommands();
+
+		{
+			VkImageSubresourceRange mipbaseRange{};
+			mipbaseRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			mipbaseRange.baseMipLevel = 0u;
+			mipbaseRange.levelCount = 1u; // The number of mipmap levels (starting from baseMipLevel) accessible to the view
+			mipbaseRange.layerCount = layerCount_;
+
+			CreateBarrier(
+				commandBuffer, 
+				currentImageLayout, 
+				VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+				VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 
+				VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+				VK_PIPELINE_STAGE_TRANSFER_BIT, VK_ACCESS_TRANSFER_READ_BIT,
+				mipbaseRange);
+		}
+
+		for (uint32_t i = 1; i < maxMipLevels; ++i)
+		{
+			VkImageBlit imageBlit{};
+
+			// Source
+			imageBlit.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlit.srcSubresource.layerCount = 6u;
+			imageBlit.srcSubresource.mipLevel = i - 1;
+			imageBlit.srcOffsets[1].x = int32_t(width >> (i - 1));
+			imageBlit.srcOffsets[1].y = int32_t(height >> (i - 1));
+			imageBlit.srcOffsets[1].z = 1;
+
+			// Destination
+			imageBlit.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlit.dstSubresource.layerCount = 6u;
+			imageBlit.dstSubresource.mipLevel = i;
+			imageBlit.dstOffsets[1].x = int32_t(width >> i);
+			imageBlit.dstOffsets[1].y = int32_t(height >> i);
+			imageBlit.dstOffsets[1].z = 1;
+
+			VkImageSubresourceRange mipSubRange = {};
+			mipSubRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			mipSubRange.baseMipLevel = i;
+			mipSubRange.levelCount = 1;
+			mipSubRange.layerCount = 6u;
+
+			//  Transiton current mip level to transfer dest
+			CreateBarrier(commandBuffer,
+				currentImageLayout, 
+				VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+				VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 
+				VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+				VK_PIPELINE_STAGE_TRANSFER_BIT, 
+				VK_ACCESS_TRANSFER_WRITE_BIT,
+				mipSubRange);
+
+			vkCmdBlitImage(
+				commandBuffer,
+				image_,
+				VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+				image_,
+				VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+				1,
+				&imageBlit,
+				VK_FILTER_LINEAR);
+
+
+			// Transition back
+			CreateBarrier(commandBuffer,
+				VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 
+				VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, // TODO maybe change to currentImageLayout?
+				VK_PIPELINE_STAGE_TRANSFER_BIT, 
+				VK_ACCESS_TRANSFER_WRITE_BIT,
+				VK_PIPELINE_STAGE_TRANSFER_BIT, 
+				VK_ACCESS_TRANSFER_READ_BIT,
+				mipSubRange);
+		}
+
+		vkDev.EndSingleTimeCommands(commandBuffer);
+	}
 
 private:
 	uint32_t FindMemoryType(VkPhysicalDevice device, uint32_t typeFilter, VkMemoryPropertyFlags properties);
