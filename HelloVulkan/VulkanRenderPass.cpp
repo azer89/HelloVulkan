430886a9175c3e1de19747ea994ccb143f62c7ef
Reza Saputra <radhitya@uwaterloo.ca>
2024-01-02 16:47:16 -0500

    done

--- HelloVulkan/VulkanRenderPass.cpp
+++ HelloVulkan/VulkanRenderPass.cpp
@@ -32,7 +32,7 @@ void VulkanRenderPass::CreateMultisampleResolveRenderPass(
 	VkAttachmentDescription singleSampledAttachment = {
 		.format = vkDev.GetSwaphchainImageFormat(),
 		.samples = VK_SAMPLE_COUNT_1_BIT,
-		.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+		.loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
 		.storeOp = VK_ATTACHMENT_STORE_OP_STORE,
 		.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
 		.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
