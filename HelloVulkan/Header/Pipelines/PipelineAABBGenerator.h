b1334fd0ebe6b6786715380d9ba48159ee2dc9d5
Reza Saputra <radhitya@uwaterloo.ca>
2024-02-04 08:11:58 -0500

    PipelineAABBGenerator

--- HelloVulkan/Header/Pipelines/PipelineAABBGenerator.h
+++ HelloVulkan/Header/Pipelines/PipelineAABBGenerator.h
@@ -3,5 +3,35 @@
 
 #include "PipelineBase.h"
 #include "VulkanDevice.h"
+#include "ClusterForwardBuffers.h"
+#include "Camera.h"
+
+class PipelineAABBGenerator final : public PipelineBase
+{
+public:
+	PipelineAABBGenerator(VulkanDevice& vkDev, ClusterForwardBuffers* cfBuffers, Camera* camera);
+	~PipelineAABBGenerator();
+
+private:
+	ClusterForwardBuffers* cfBuffers_;
+	Camera* camera_;
+
+	VulkanBuffer cfUBOBuffer_;
+	VkDescriptorSet computeDescriptorSet_;
+	VkPipeline pipeline_;
+
+private:
+	void Execute(VulkanDevice& vkDev, VkCommandBuffer commandBuffer);
+
+	void CreateDescriptorPool(VkDevice device);
+	void CreateDescriptorLayout(VkDevice device);
+
+	void AllocateDescriptorSet(VkDevice device);
+	void UpdateDescriptorSet(VkDevice device, VulkanBuffer* aabbBuffer);
+
+	void CreateComputePipeline(
+		VkDevice device,
+		VkShaderModule computeShader);
+};
 
 #endif
--- HelloVulkan/Source/Pipelines/PipelineAABBGenerator.cpp
+++ HelloVulkan/Source/Pipelines/PipelineAABBGenerator.cpp
@@ -1,1 +1,20 @@
 #include "PipelineAABBGenerator.h"
+
+PipelineAABBGenerator::PipelineAABBGenerator(
+	VulkanDevice& vkDev, 
+	ClusterForwardBuffers* cfBuffers,
+	Camera* camera) :
+	PipelineBase(vkDev,
+	{
+		.type_ = PipelineType::Compute
+	}),
+	cfBuffers_(cfBuffers),
+	camera_(camera)
+{
+}
+
+
+PipelineAABBGenerator::~PipelineAABBGenerator()
+{
+
+}
