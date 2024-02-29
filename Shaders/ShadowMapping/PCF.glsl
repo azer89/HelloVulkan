58581083e1b57822c977b4ccebfac2c18efe0df4
Reza Saputra <reza.adhitya.saputra@gmail.com>
2024-02-29 15:43:45 -0500

    Shadow_Mapping_Improvements (#85)
    * faster shader
    
    * reorganize
    
    * revert

---
+++ Shaders/ShadowMapping/PCF.glsl
@@ -0,0 +1,28 @@
+float ShadowPCF(vec4 sc)
+{
+	ivec2 texDim = textureSize(shadowMap, 0);
+	float scale = shadowUBO.pcfScale;
+	float dx = scale * 1.0 / float(texDim.x);
+	float dy = scale * 1.0 / float(texDim.y);
+
+	vec3 N = normalize(normal);
+	vec3 lightDir = normalize(shadowUBO.lightPosition.xyz - worldPos);
+	float bias = max(shadowUBO.shadowMaxBias * (1.0 - dot(N, lightDir)), shadowUBO.shadowMinBias);
+
+	// TODO Very slow, consider change to a constant for better performance
+	//const int range = 2;
+	int range = int(shadowUBO.pcfIteration);
+	float shadow = 0.0;
+	int count = 0;
+	for (int x = -range; x <= range; x++)
+	{
+		for (int y = -range; y <= range; y++)
+		{
+			vec2 off = vec2(dx * x, dy * y);
+			float dist = texture(shadowMap, sc.st + off).r;
+			shadow += (sc.z - bias) > dist ? 0.1 : 1.0;
+			count++;
+		}
+	}
+	return shadow / count;
+}
