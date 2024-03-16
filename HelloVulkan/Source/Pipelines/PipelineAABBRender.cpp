745120fcf8c3d44968a130c108e187f51645c870
Reza Saputra <radhitya@uwaterloo.ca>
2024-03-16 14:34:55 -0400

    wip

---
+++ HelloVulkan/Source/Pipelines/PipelineAABBRender.cpp
@@ -0,0 +1,32 @@
+#include "PipelineAABBRender.h"
+#include "ResourcesShared.h"
+#include "Scene.h"
+
+PipelineAABBRender::PipelineAABBRender(
+	VulkanContext& ctx,
+	Scene* scene,
+	ResourcesShared* resShared,
+	uint8_t renderBit = 0u) :
+	PipelineBase(ctx,
+	{
+		.type_ = PipelineType::GraphicsOffScreen,
+		.msaaSamples_ = resShared->multiSampledColorImage_.multisampleCount_,
+		.depthTest_ = true,
+		.depthWrite_ = false
+	}
+	)
+{
+
+}
+
+PipelineAABBRender::~PipelineAABBRender()
+{
+}
+
+void PipelineAABBRender::FillCommandBuffer(VulkanContext& ctx, VkCommandBuffer commandBuffer)
+{
+}
+
+void PipelineAABBRender::CreateDescriptor(VulkanContext& ctx)
+{
+}
