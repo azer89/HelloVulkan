f6d37b133ec5639d202c228d187a6bc530b9e4e8
Reza Saputra <radhitya@uwaterloo.ca>
2024-03-15 20:12:09 -0400

    revert

--- HelloVulkan/Source/Pipelines/PipelineLine.cpp
+++ HelloVulkan/Source/Pipelines/PipelineLine.cpp
@@ -87,11 +87,11 @@ void PipelineLine::ProcessScene(VulkanContext& ctx)
 	// Build bounding boxes
 	for (size_t i = 0; i < scene_->originalBoundingBoxes_.size(); ++i)
 	{
-		//const MeshData& mData = scene_->meshDataArray_[i];
-		//const glm::mat4& mat = scene_->modelUBOs_[mData.modelIndex].model;
-		const BoundingBox& box = scene_->transformedBoundingBoxes_[i];
+		const MeshData& mData = scene_->meshDataArray_[i];
+		const glm::mat4& mat = scene_->modelUBOs_[mData.modelIndex].model;
+		const BoundingBox& box = scene_->originalBoundingBoxes_[i];
 		AddBox(
-			glm::translate(glm::mat4(1.f), 0.5f * (box.min_ + box.max_)), // mat
+			mat * glm::translate(glm::mat4(1.f), 0.5f * (box.min_ + box.max_)), // mat
 			0.5f * glm::vec3(box.max_ - box.min_), // size
 			BOX_COLOR);
 	}
