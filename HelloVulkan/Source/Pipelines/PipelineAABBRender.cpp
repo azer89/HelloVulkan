b6ad5b3d08ee71ba184767c392dd1bb64cc609fa
Reza Saputra <radhitya@uwaterloo.ca>
2024-03-16 16:02:50 -0400

    wip

--- HelloVulkan/Source/Pipelines/PipelineAABBRender.cpp
+++ HelloVulkan/Source/Pipelines/PipelineAABBRender.cpp
@@ -14,9 +14,31 @@ PipelineAABBRender::PipelineAABBRender(
 		.depthTest_ = true,
 		.depthWrite_ = false
 	}),
-	scene_(scene)
+	scene_(scene),
+	shouldRender_(false)
 {
-
+	CreateMultipleUniformBuffers(ctx, cameraUBOBuffers_, sizeof(CameraUBO), AppConfig::FrameCount);
+	renderPass_.CreateOffScreenRenderPass(ctx, renderBit, config_.msaaSamples_);
+	framebuffer_.CreateResizeable(
+		ctx,
+		renderPass_.GetHandle(),
+		{
+			&(resShared->multiSampledColorImage_),
+			&(resShared->depthImage_)
+		},
+		IsOffscreen()
+	);
+	CreateDescriptor(ctx);
+	CreatePipelineLayout(ctx, descriptor_.layout_, &pipelineLayout_);
+	CreateGraphicsPipeline(ctx,
+		renderPass_.GetHandle(),
+		pipelineLayout_,
+		{
+			AppConfig::ShaderFolder + "AABB/AABB.vert",
+			AppConfig::ShaderFolder + "AABB/AABB.frag",
+		},
+		&pipeline_
+	);
 }
 
 PipelineAABBRender::~PipelineAABBRender()
@@ -25,8 +47,43 @@ PipelineAABBRender::~PipelineAABBRender()
 
 void PipelineAABBRender::FillCommandBuffer(VulkanContext& ctx, VkCommandBuffer commandBuffer)
 {
+	if (!shouldRender_)
+	{
+		return;
+	}
+
+	TracyVkZoneC(ctx.GetTracyContext(), commandBuffer, "AABB", tracy::Color::Orange4);
+
+	const uint32_t frameIndex = ctx.GetFrameIndex();
+	renderPass_.BeginRenderPass(ctx, commandBuffer, framebuffer_.GetFramebuffer());
+	BindPipeline(ctx, commandBuffer);
+	vkCmdBindDescriptorSets(
+		commandBuffer,
+		VK_PIPELINE_BIND_POINT_GRAPHICS,
+		pipelineLayout_,
+		0,
+		1,
+		&descriptorSets_[frameIndex],
+		0,
+		nullptr);
+	vkCmdDraw(commandBuffer, 36, scene_->transformedBoundingBoxes_.size(), 0, 0);
+	vkCmdEndRenderPass(commandBuffer);
 }
 
 void PipelineAABBRender::CreateDescriptor(VulkanContext& ctx)
 {
+	constexpr uint32_t frameCount = AppConfig::FrameCount;
+
+	VulkanDescriptorInfo dsInfo;
+	dsInfo.AddBuffer(nullptr, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT); // 0
+	dsInfo.AddBuffer(&(scene_->transformedBoundingBoxBuffer_), VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_VERTEX_BIT); // 1
+
+	// Create pool and layout
+	descriptor_.CreatePoolAndLayout(ctx, dsInfo, frameCount, 1u);
+
+	for (size_t i = 0; i < frameCount; ++i)
+	{
+		dsInfo.UpdateBuffer(&(cameraUBOBuffers_[i]), 0);
+		descriptor_.CreateSet(ctx, dsInfo, &(descriptorSets_[i]));
+	}
 }
--- Shaders/AABB/AABBRender.vert
+++ Shaders/AABB/AABBRender.vert
@@ -1,6 +1,18 @@
 #version 460 core
 
+#include <Cube.glsl>
+#include <CameraUBO.glsl>
+#include <AABB/AABB.glsl>
+
+layout(set = 0, binding = 0)  uniform CameraBlock { CameraUBO camUBO; };
+layout(set = 0, binding = 1) buffer Boxes { AABB boxes[]; };
+
 void main()
 {
+	AABB box = boxes[gl_InstanceIndex];
+	float extents = length(box.maxPoint - box.minPoint) * 0.5;
+	int idx = CUBE_INDICES[gl_VertexIndex];
+	vec4 pos4 = vec4(CUBE_POS[idx] * extents, 1.0);
+	gl_Position = camUBO.projection * camUBO.view * pos4;
 
 }
