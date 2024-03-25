7b8394ade48e845801c1f2b9db639c86e9d48115
Reza Saputra <radhitya@uwaterloo.ca>
2024-03-25 05:52:52 -0400

    input context

--- HelloVulkan/Header/InputContext.h
+++ HelloVulkan/Header/InputContext.h
@@ -9,11 +9,22 @@
 struct InputContext
 {
 public:
-	// TODO Initialize these structs
-	CameraUBO cameraUBO_ = {};
-	PushConstPBR pbrPC_ = {};
+	PushConstPBR pbrPC_ = 
+	{
+		.lightIntensity = 1.5f,
+		.baseReflectivity = 0.01f,
+		.maxReflectionLod = 1.f,
+		.lightFalloff = 0.1f,
+		.albedoMultipler = 0.1f
+	};
 
 	std::array<float, 3> shadowCasterPosition_ = {0.f, 0.f, 0.f};
+	float shadowNearPlane_ = 0.1f;
+	float shadowFarPlane_ = 50.f;
+	float shadowOrthoSize_ = 12.5f;
+	float shadowMinBias_ = 0.001f;
+	float shadowMaxBias_ = 0.001f;
+
 	bool renderInfiniteGrid_ = true;
 	bool renderAABB_ = false;
 	bool renderLights_ = true;
