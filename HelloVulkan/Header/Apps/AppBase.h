aa55404eadba9c292bddc1e8dea089f4ebf78aec
Reza Saputra <radhitya@uwaterloo.ca>
2024-04-02 10:29:25 -0400

    added requires

--- HelloVulkan/Header/Apps/AppBase.h
+++ HelloVulkan/Header/Apps/AppBase.h
@@ -58,10 +58,9 @@ protected:
 	void InitSharedResources();
 
 	template<class T, class... U>
+	requires (std::is_base_of<PipelineBase, T>::value)
 	T* AddPipeline(U&&... u)
 	{
-		static_assert(std::is_base_of<PipelineBase, T>::value, "T must be the derived class of PipelineBase");
-
 		// Create std::unique_ptr of Pipeline
 		std::unique_ptr<T> pipeline = std::make_unique<T>(std::forward<U>(u)...);
 		T* ptr = pipeline.get();
@@ -70,10 +69,9 @@ protected:
 	}
 
 	template<class T, class... U>
+	requires (std::is_base_of<ResourcesBase, T>::value)
 	T* AddResources(U&&... u)
 	{
-		static_assert(std::is_base_of<ResourcesBase, T>::value, "T must be the derived class of ResourcesBase");
-
 		// Create std::unique_ptr of Resources
 		std::unique_ptr<T> resources = std::make_unique<T>(std::forward<U>(u)...);
 		T* ptr = resources.get();
@@ -88,11 +86,11 @@ protected:
 	std::unique_ptr<Camera> camera_ = nullptr;
 
 	// Timing
-	FrameCounter frameCounter_;
+	FrameCounter frameCounter_ = {};
 
 	// These two are not copyable or movable
-	VulkanInstance vulkanInstance_;
-	VulkanContext vulkanContext_;
+	VulkanInstance vulkanInstance_ = {};
+	VulkanContext vulkanContext_ = {};
 
 	// A list of pipelines (graphics, compute, or raytracing)
 	std::vector<std::unique_ptr<PipelineBase>> pipelines_ = {};
@@ -101,9 +99,9 @@ protected:
 	std::vector<std::unique_ptr<ResourcesBase>> resources_ = {};
 
 	// Window size
-	uint32_t windowWidth_;
-	uint32_t windowHeight_;
-	bool shouldRecreateSwapchain_;
+	uint32_t windowWidth_ = 0;
+	uint32_t windowHeight_ = 0;
+	bool shouldRecreateSwapchain_ = true;
 
 	ResourcesShared* resourcesShared_ = nullptr;
 	ResourcesIBL* resourcesIBL_ = nullptr;
